9192c6a17eb02dd95227ca5bf7dc85e399f179c7
Wallace Rio <wallrio@gmail.com>
2018-12-09 02:29:53 -0200

    implementation sub-member of services

--- src/CreateServer.php
+++ src/CreateServer.php
@@ -12,15 +12,35 @@ class CreateServer{
 		$this->serverList = $numargs;	
 		foreach ($numargs as $key => $value) {
 			$serverCurrent = $value;
+
+			
+
 			if( isset($serverCurrent->namespace) ){
 				$namespace = $serverCurrent->namespace;
 				$this->$namespace = $value;
 				$serverCurrent->server = $this;
+
+				
+
 			}
 		}
 	}
 	
+
+	public function __get($name) {
+
+		foreach ($this->serverList as $key => $value) {				
+			if( isset($value->namespace) ){
+				return $value->$name;
+			}else{
+				die('Method not exist ['.$method.']');				
+			}
+		}
+		
+    }
+
 	public function __call($method,$arguments){		
+		
 		foreach ($this->serverList as $key => $value) {				
 			if( !isset($value->namespace) ){
 				return call_user_func_array(array($value,$method), $arguments);
